08faf123916418157edd10f8c7b676f699b479b6
Carlos Santos <carlosrsantos2001@gmail.com>
2024-03-07 15:41:53 -0300

    feat: new contact buttons

--- src/components/layout/home-lp/index.tsx
+++ src/components/layout/home-lp/index.tsx
@@ -1,4 +1,5 @@
 "use client";
+import CustomButton from "@/components/common/custom-button";
 import { Params } from "@/domain/model/params";
 import Image from "next/image";
 import { useTranslation } from "react-i18next";
@@ -43,9 +44,45 @@ const HomeLP: React.FC<HomeLPProps> = ({ params: { locale } }) => {
                 FIAP↗
               </a>
             </span>
+            {/* <span className="text-scorpionGrey">
+              {t("bootcamp")}{" "}
+              <a
+                href="https://www.fiap.com.br/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blueKenzie"
+              >
+                Kenzie Academy↗
+              </a>
+            </span> */}
+            {/* <span className="text-scorpionGrey">
+              {t("technical")}{" "}
+              <a
+                href="https://www.fiap.com.br/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-greenIFSP"
+              >
+                IFSP↗
+              </a>
+            </span> */}
+            <div className="flex mt-4 gap-3">
+              <CustomButton
+                icon="/assets/icons/github.svg"
+                color="red"
+                href="https://github.com/carlossantos01"
+                alt="GitHub"
+              />
+              <CustomButton
+                icon="/assets/icons/linkedin.svg"
+                color="red"
+                href="https://www.linkedin.com/in/carloseorsantos/"
+                alt="linkedin"
+              />
+            </div>
           </div>
         </div>
-        <div className="mt-6">
+        <div className="mt-6 md:mt-0">
           <Image
             src="/assets/images/gif.gif"
             className="mix-blend-multiply"
